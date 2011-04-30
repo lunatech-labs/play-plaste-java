31b518ad584034e2e8312688e209fc0dada4a594
Francisco José Canedo Dominguez <paco+git@lunatech.com>
2011-04-30 11:02:18 +0200

    Added 404 for non-existing pastes
    …as per review

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -22,6 +22,7 @@ public class Application extends Controller {
 
 	public static void show(Long id) {
 		final Paste paste = Paste.findById(id);
+		notFoundIfNull(paste);
 		final String highlightedCode = Pygments.highlight(paste.code, paste.codeMimeType);
 		render(paste, highlightedCode);
 	}
